03e5a0a1ce6a0deb2f750f561f9841b73bdeeb98
Aman Kumar Prasad <amanprasad1502@gmail.com>
2024-01-27 00:13:18 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+const swiper = new Swiper('.swiper', {
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+
+    autoplay: {
+        delay: 2000, 
+    }
+  
+  
+  });
+
+  document.addEventListener('DOMContentLoaded', function () {
+    var mySwiper = new Swiper('.swiper', {
+        // your Swiper configuration options here
+    });
+
+    // Change Swiper pagination color
+    var paginationBullets = document.querySelectorAll('.swiper-pagination-bullet');
+    var activeBullet = document.querySelector('.swiper-pagination-bullet-active');
+
+    paginationBullets.forEach(function (bullet) {
+        bullet.style.backgroundColor = '#BD2941';
+    });
+
+    if (activeBullet) {
+        activeBullet.style.backgroundColor = '#C8102E';
+    }
+});
